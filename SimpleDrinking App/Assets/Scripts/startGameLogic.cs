7d5a10f47ebdd0883bca27ec9c85deb2caec8dfa
karlpedro <162471444+karlpedro@users.noreply.github.com>
2024-06-08 14:40:21 +0200

    Add Player button

--- SimpleDrinking App/Assets/Scripts/startGameLogic.cs
+++ SimpleDrinking App/Assets/Scripts/startGameLogic.cs
@@ -2,55 +2,60 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using TMPro.Examples;
+using System.Collections.Generic;
 
 public class StartScreenController : MonoBehaviour
 {
-    public TMP_InputField inputField1, inputField2, inputField3, inputField4, inputField5,
-    inputField6, inputField7, inputField8, inputField9, inputField10;
-    private int numberOfInputFields = 10;
-    int numberOfNames = 0;
-            private TMP_InputField[] allFields;
+    public TMP_InputField inputField;
+    public TMP_Text playerListText;
+    public Button addPlayerButton;
+    public Button startButton;
+    private List<string> playerNames = new List<string>();
 
-    // Method to be called when the Start Button is pressed
-    public void StartGame()
-    {   
-        SetupTextFields();
-        SetupPlayers();
-        if(numberOfNames>2) {
-            Screen.orientation = ScreenOrientation.LandscapeLeft;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 );
+    void Start()
+    {
+        addPlayerButton.onClick.AddListener(HandleButtonSubmit);
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    private void HandleButtonSubmit()
+    {
+        string input = inputField.text;
+        if (!string.IsNullOrEmpty(input))
+        {
+            AddPlayer(input);
+            inputField.text = ""; // Clear the input field after adding
+            inputField.ActivateInputField(); // Reactivate the input field for new input
         }
     }
 
-    private void SetupPlayers() {
-        for(var i=0; i < numberOfInputFields; i++) {
-            string playerName = allFields[i].text;
-            if(playerName.Length!=0) {
-                numberOfNames++;
-            }
-        }
-        PlayerLogic.InitiatePlayers(numberOfNames);
-        for(var i=0; i < numberOfInputFields; i++) {
-            string playerName = allFields[i].text;
-            if(playerName.Length!=0) {
+    private void AddPlayer(string playerName)
+    {
+        playerNames.Add(playerName);
+        UpdatePlayerList();
+    }
+
+    private void UpdatePlayerList()
+    {
+        playerListText.text = string.Join("\n", playerNames);
+    }
+
+    public void StartGame()
+    {
+        if (playerNames.Count > 2)
+        {
+            PlayerLogic.InitiatePlayers(playerNames.Count);
+            foreach (var playerName in playerNames)
+            {
                 PlayerLogic.AddPlayer(playerName);
             }
-        }
-    }
 
-    private void SetupTextFields() {
-        allFields = new TMP_InputField[numberOfInputFields];
-    
-        allFields[0] = inputField1;
-        allFields[1] = inputField2;
-        allFields[2] = inputField3;
-        allFields[3] = inputField4;
-        allFields[4] = inputField5;
-        allFields[5] = inputField6;
-        allFields[6] = inputField7;
-        allFields[7] = inputField8;
-        allFields[8] = inputField9;
-        allFields[9] = inputField10;
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            Debug.Log("At least 3 players are required to start the game.");
+        }
     }
 }
